0ec9877161f3c6feebe1041d6e2d32b3af1e5221
Nelson Pupiales <nelsonpupiales@gmail.com>
2020-03-22 21:51:25 -0500

    primer commit

---
+++ src/main/java/com/nelsonpupiales/WSLaboratorio/WsLaboratorioApplication.java
@@ -0,0 +1,13 @@
+package com.nelsonpupiales.WSLaboratorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsLaboratorioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsLaboratorioApplication.class, args);
+	}
+
+}
